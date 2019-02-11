7479619abc379698e6c8b4337e44563bdcaa25b7
Rattapong Khamphansa <rkdev@Rattapongs-MacBook-Pro.local>
2019-02-11 12:12:11 +0700

    change round date format

--- api.lotery-th/services/lottery-service.js
+++ api.lotery-th/services/lottery-service.js
@@ -16,15 +16,34 @@ exports.get_lottery_rounds = async function get_lottery_rounds() {
     return [rounds];
 }
 
-const lotto_regex = /^\/lotto\//
 function scraping_lotto_rounds($) {
     let rounds = [];
     $('option').each(function (i, elem) {
-        let round = {
-            text: $(this).text() ? $(this).text() : '',
-            value: $(this).attr('value') ? $(this).attr('value').replace(lotto_regex, '') : '',
-        }
+        let round = $(this).attr('value') ? lotto_to_valid_date($(this).attr('value')) : '';
         rounds.push(round);
     });
     return rounds;
+}
+
+const lotto_regex = /^\/lotto\//
+
+function lotto_to_valid_date(value) {
+    try {
+        let date = value.replace(lotto_regex, '');
+        let date_arr = date.split('-');
+        let day = date_arr[0].padStart(2, '0');
+        let month = date_arr[1];
+        let buddhist_year = date_arr[2];
+        let christian_year = to_christian_year(buddhist_year)
+        return `${christian_year}-${month}-${day}`;
+    } catch (error) {
+        console.log(error);
+        return value;
+    }
+}
+
+function to_christian_year(buddhist_year) {
+    let full_buddist_year = buddhist_year.padStart(4, '25')
+    let christian_year = parseInt(full_buddist_year) - 543;
+    return christian_year;
 }
